f4565ac43a63017aba676ff022d1d04ba6cb2322
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-11-22 10:22:02 -0600

    feat: app base

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+const { trainers } = require('../data/trainers');
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Servidor trainers runging in ${port}...`);
+});
+
+app.get('/', (req, res) => {
+  res.write('<h1>Bienvenido a trainers API!</h1>');
+});
+
+// GET: Obtener todos los pokemons
+app.get('/trainers', (req, res) => {
+
+  const name = req.query.name || '';
+
+
+
+  const isChampion = req.query.isChampion === 'true' ? true : false
+
+  const isLikeName = ((trainer) => {
+    return trainer.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+  });
+
+  const hasWinnedTournaments = ((trainer) => {
+    return trainer.winnedTournaments.length > 0;
+  });
+
+
+  let data = trainers.filter(isLikeName);
+
+  if (isChampion) {
+    data = data.filter(hasWinnedTournaments);
+  }
+
+  res.json(data);
+});
