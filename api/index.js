c64aca3e9b928417129ae8e51eec25b3b7769df7
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-11-22 21:56:03 -0600

    test api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Api trainers running on ${PORT}... API`);
+    console.log(`Api trainers running on ${PORT}... API`);
 });
 
 module.exports = app;
